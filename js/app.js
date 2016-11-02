86ebfd25af445bc520cf8e2d51f4a77e1e41f454
Arandi López <arandilopez.93@gmail.com>
2016-11-01 20:30:16 -0600

    new 24h counter for the lolz

--- js/app.js
+++ js/app.js
@@ -6,11 +6,33 @@
     return value.toString();
   });
 
-  new Vue({
-    el: '#app',
-    data: {
-      date: Math.trunc(new Date(Date.UTC(2016, 10, 4, 24, 0, 0)).getTime() / 1000),
-      now: Math.trunc((new Date()).getTime() / 1000)
+  var Down = {
+    props:['date', 'now'],
+    data: function () {
+      return {
+        nombre: 'Arandi'
+      }
+    },
+    computed: {
+      seconds: function () {
+        return (this.date - this.now) % 60;
+      },
+
+      minutes: function () {
+        return Math.trunc((this.date - this.now) / 60) % 60;
+      },
+
+      hours: function () {
+        return Math.trunc((this.date - this.now) / 60 / 60) % 24;
+      },
+    },
+  };
+
+  var First = {
+    props: ['date', 'now'],
+    data: function () {
+      return {
+      }
     },
     computed: {
       seconds: function () {
@@ -31,9 +53,36 @@
 
       days_text: function () {
         return (this.days == 1) ? 'día' : 'días';
+      },
+    },
+  }
+
+  new Vue({
+    el: '#app',
+    data: {
+      // date: Math.trunc(new Date(Date.UTC(2016, 10, 1, 24, 0, 0)).getTime() / 1000),
+      date: Math.trunc(new Date("2016-11-05").getTime() / 1000),
+      finish_date: Math.trunc(new Date("2016-11-06").getTime() / 1000),
+      now: Math.trunc((new Date()).getTime() / 1000),
+      started: false
+    },
+    computed: {
+      has_begun: function () {
+        return !(this.now < this.date);
       }
     },
+    components: {
+      'DownCounter': Down,
+      'First': First,
+    },
     created: function () {
+      firebase.database()
+        .ref('/data')
+        .on('value', function (snap) {
+          this.finish_date = Math.trunc(new Date(snap.val().finished_date).getTime() / 1000);
+          this.started = snap.val().started;
+        }.bind(this));
+
       setInterval(function () {
         this.now = Math.trunc((new Date()).getTime() / 1000);
       }.bind(this), 1000);
